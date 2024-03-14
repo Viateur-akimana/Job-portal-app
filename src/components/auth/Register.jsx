695a3f985ad1c2260fae8daee0adfe61e99b6b8f
Viateur <akimanaviateur94@gmail.com>
2024-03-14 21:10:10 +0200

    here are changes

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -15,7 +15,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:3000/api/auth/register", {
+      const response = await axios.post("http://localhost:3000/api/auth/rer", {
         username,
         email,
         password
